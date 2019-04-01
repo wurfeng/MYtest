c18fa721e77ac35f69d12e4c2301ffb1641a868f
wurfeng <1126823257@qq.com>
2019-04-01 16:59:40 +0800

    修改测试2

---
+++ version1/src/main/java/test2.java
@@ -0,0 +1,20 @@
+/**
+ * @ClassName test2
+ * @Author wrf.
+ * @date 2019/4/1.16:19.
+ * @Version 1.0
+ */
+public class test2 {
+
+    /*
+     * @Author chengpunan
+     * @Description //TODO
+     * @Date 16:55 2019/4/1
+     * @Param [i]
+     * @return int
+     **/
+    private int anInt(int i){
+
+        return 1;
+    }
+}
